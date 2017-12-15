391ad1bba6d32f048bad3392d5db54d29a5d5485
ymkj <ymkj@ymkjdeiMac.local>
2017-12-15 10:24:14 +0800

    Initial Commit

---
+++ uicollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  uicollection
+//
+//  Created by ymkj on 2017/12/15.
+//  Copyright © 2017年 ymkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ uicollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  uicollection
+//
+//  Created by ymkj on 2017/12/15.
+//  Copyright © 2017年 ymkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
